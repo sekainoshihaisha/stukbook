f87f0a47f05c9fa3bf11d810fd167ece601aaffc
Brian Tellez <brianulises0197@gmail.com>
2016-12-26 05:39:02 +0000

    Added timeline

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     end
   end
   resources :posts, only: [:create, :edit, :update, :destroy]
+  resources :activities, only: [:index]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
